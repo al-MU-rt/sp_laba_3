c53ee719d8b20a2392ce4f289c25bcdd647f7a0b
al-MU-rt <anton25348@gmail.com>
2023-12-26 19:59:16 +0200

    лаба 4

---
+++ cpp_4/.laba_4.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/anton/repos/busybox/cpp_4/laba_4.ko := ccache arm-linux-gnueabihf-ld -r  -EL -z noexecstack  -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/anton/repos/busybox/cpp_4/laba_4.ko /home/anton/repos/busybox/cpp_4/laba_4.o /home/anton/repos/busybox/cpp_4/laba_4.mod.o ;  true
---
+++ cpp_4/.tmp_versions/laba_4.mod
@@ -0,0 +1,3 @@
+/home/anton/repos/busybox/cpp_4/laba_4.ko
+/home/anton/repos/busybox/cpp_4/laba_4.o
+
